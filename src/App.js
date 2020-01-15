3bb23dc6b135dc4f9a97eb7c8f7242222cfcfe40
Nicole Marty <nmarty310@gmail.com>
2020-01-15 15:34:53 -0600

    messing around with different libraries and nav bars

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import logo from './logo.jpg'
 import AppContainer from './components/AppContainer'
+import TopNav from './components/TopNav'
+import Footer from './components/Footer'
 import './App.css'
 
 function App () {
   return (
     <div className='App'>
       <AppContainer>
-        <div className='home'>
-          <img src={logo} className='App-logo' alt='logo' />
+        <div className='div'>
+          <TopNav />
+          <div className='home'>
+            <img src={logo} className='App-logo' alt='logo' />
+          </div>
+          <Footer />
         </div>
       </AppContainer>
     </div>
