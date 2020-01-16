599da0535f0b011c1646ba1e24b98d856e1ca801
Nicole Marty <nmarty310@gmail.com>
2020-01-16 16:11:47 -0600

    going back to using materialize first since first full stack app I'm building on my own. bit off more than I can chew with also learning a new css library

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
 import React from 'react'
-import logo from './logo.jpg'
-import AppContainer from './components/AppContainer'
-import TopNav from './components/TopNav'
-import Footer from './components/Footer'
 import './App.css'
 
 function App () {
-  return (
-    <div className='App'>
-      <AppContainer>
-        <div className='div'>
-          <TopNav />
-          <div className='home'>
-            <img src={logo} className='App-logo' alt='logo' />
-          </div>
-          <Footer />
-        </div>
-      </AppContainer>
-    </div>
-  )
+  return <div className='App'></div>
 }
 
 export default App
--- src/components/AppContainer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function AppContainer (props) {
-  return <main {...props} />
-}
-
-export default AppContainer
