739254d195eb36d96795ccbf6fbf48273ad8b661
Nicole Marty <nmarty310@gmail.com>
2020-01-18 16:58:58 -0600

    restructured under client folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import SideNav from './components/SideNav'
+import TopNav from './components/TopNav'
 import AppContainer from './components/AppContainer'
 import About from './pages/About'
 import './App.css'
@@ -11,7 +11,8 @@ class App extends Component {
     return (
       <Router>
         <div>
-          <Route render={history => <SideNav history={history} />} />
+          <TopNav />
+          {/*<Route render={history => <SideNav history={history} />} />*/}
           <AppContainer>
             <Route exact path='/' component={Home} />
             <Route exact path='/Home' component={Home} />
