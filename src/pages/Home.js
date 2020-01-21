913d230811ab4940f94bf8be6ada752d215a94e6
Nicole Marty <nmarty310@gmail.com>
2020-01-21 08:25:57 -0600

    fixed the node modules issue and the page routing using Link from react-dom-router

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import M from 'materialize-css'
 
-class Home extends React.Component {
-  componentDidMount () {
-    M.AutoInit()
-    M.Toast.dismissAll()
-  }
-
-  redirect = location => {
-    this.props.history.push(location)
-  }
-
-  render () {
-    return <div className='row'>Home</div>
-  }
+function Home () {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>
+        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
+        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
+        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
+        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
+        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
+        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+      </p>
+    </div>
+  )
 }
 
 export default Home
